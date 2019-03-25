53d8a7a78501059e37509f0a020ce142ed772ced
(cdep)illabout <cdep.illabout@gmail.com>
2019-03-25 13:29:43 +0900

    Add more functions for working with passwords.

--- src/Data/Password.hs
+++ src/Data/Password.hs
@@ -1,43 +1,163 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Data.Password
-  ( Pass(..)
+  (
+    -- * Plaintext Password
+    Pass(..)
+    -- * Hashed Password
   , PassHash(..)
   , Salt(..)
+    -- * Functions for Hashing Plaintext Passwords
   , hashPass
   , hashPassWithSalt
-
+  , newSalt
+    -- * Functions for Checking Plaintext Passwords Against Hashed Passwords
+  , checkPass
+  , -- * Setup for doctests.
+    -- $setup
   ) where
 
-import Crypto.Scrypt (Salt(..), encryptPassIO)
+import Control.Monad.IO.Class (MonadIO(liftIO))
+import Crypto.Scrypt (EncryptedPass(..), Salt(..), defaultParams, encryptPass,
+                      newSalt, verifyPass')
 import qualified Crypto.Scrypt as Scrypt
+import Data.String (IsString)
 import Data.Text (Text)
+import Data.Text.Encoding (decodeUtf8With, encodeUtf8)
+import Data.Text.Encoding.Error (lenientDecode)
 
-data Pass = Pass
+-- $setup
+-- >>> :set -XOverloadedStrings
+--
+-- Import needed libraries.
+--
+-- >>> import Data.ByteString (pack)
+-- >>> import Test.QuickCheck (Arbitrary(arbitrary), vector)
+-- >>> import Test.QuickCheck.Instances.ByteString ()
+-- >>> import Test.QuickCheck.Instances.Text ()
+--
+-- 'Arbitrary' instances for types exported from this library.
+--
+-- >>> instance Arbitrary Salt where arbitrary = Salt . pack <$> vector 32
+-- >>> instance Arbitrary Pass where arbitrary = fmap Pass arbitrary
+-- >>> instance Arbitrary PassHash where arbitrary = hashPassWithSalt <$> arbitrary <*> arbitrary
+--
+-- 'Arbitrary' instances for types exported from "Crypto.Scrypt".
+--
+-- >>> instance Arbitrary Scrypt.Pass where arbitrary = fmap Scrypt.Pass arbitrary
+-- >>> instance Arbitrary EncryptedPass where arbitrary = encryptPass defaultParams <$> arbitrary <*> arbitrary
+
+newtype Pass = Pass
   { unPass :: Text
   } deriving (Eq, IsString, Ord, Read, Show)
 
-data PassHash = PassHash
+newtype PassHash = PassHash
   { unPassHash :: Text
   } deriving (Eq, Ord, Read, Show)
 
--- | Convert an 'Scrypt.Pass' to our 'Pass' type.
+-- | Convert an "Crypto.Scrypt".'Scrypt.Pass' to our 'Pass' type.
 --
 -- >>> passToScryptPass $ Pass "foobar"
--- hello
+-- Pass {getPass = "foobar"}
 passToScryptPass :: Pass -> Scrypt.Pass
 passToScryptPass (Pass pass) = Scrypt.Pass $ encodeUtf8 pass
 
-scryptEncryptedPassToPassHash :: EncryptedPass -> PassHash
-scryptEncryptedPassToPassHash (EncryptedPass encryptPass) =
-  PassHash $ decodeUtf8 encryptPass
+-- Convert our 'Pass' type to an "Crypto.Scrypt".'Script.Pass'.
+--
+-- Opposite of 'passToScryptPass'.
+--
+-- >>> scryptPassToPass $ Scrypt.Pass "foobar"
+-- Pass {getPass = "foobar"}
+-- scryptPassToPass :: Scrypt.Pass -> Pass
+-- scryptPassToPass (Scrypt.Pass pass) = Pass $ decodeUtf8With lenientDecode pass
 
+-- | Convert an "Crypto.Scrypt".'EncryptedPass' to our 'PassHash' type.
+--
+-- This is the opposite of 'passHashToScryptEncryptedPass'.
+--
+-- >>> let salt = Salt "abcdefghijklmnopqrstuvwxyz012345"
+-- >>> let pass = Scrypt.Pass "foobar"
+-- >>> let encryptedPass = encryptPass defaultParams salt pass
+-- >>> encryptedPass
+-- EncryptedPass {getEncryptedPass = "14|8|1|YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXowMTIzNDU=|nENDaqWBmPKapAqQ3//H0iBImweGjoTqn5SvBS8Mc9FPFbzq6w65maYPZaO+SPamVZRXQjARQ8Y+5rhuDhjIhw=="}
+-- >>> scryptEncryptedPassToPassHash encryptedPass
+-- PassHash {unPassHash = "14|8|1|YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXowMTIzNDU=|nENDaqWBmPKapAqQ3//H0iBImweGjoTqn5SvBS8Mc9FPFbzq6w65maYPZaO+SPamVZRXQjARQ8Y+5rhuDhjIhw=="}
+--
+-- prop> scryptEncryptedPassToPassHash (passHashToScryptEncryptedPass passHash) == passHash
+scryptEncryptedPassToPassHash :: EncryptedPass -> PassHash
+scryptEncryptedPassToPassHash (EncryptedPass encPass) =
+  PassHash $ decodeUtf8With lenientDecode encPass
+
+-- | Convert out 'PassHash' type to "Crypto.Scrypt".'EncryptedPass'.
+--
+-- This is the opposite of 'scryptEncryptedPassToPassHash'.
+--
+-- >>> let salt = Salt "abcdefghijklmnopqrstuvwxyz012345"
+-- >>> let pass = Scrypt.Pass "foobar"
+-- >>> let encryptedPass = encryptPass defaultParams salt pass
+-- >>> encryptedPass
+-- EncryptedPass {getEncryptedPass = "14|8|1|YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXowMTIzNDU=|nENDaqWBmPKapAqQ3//H0iBImweGjoTqn5SvBS8Mc9FPFbzq6w65maYPZaO+SPamVZRXQjARQ8Y+5rhuDhjIhw=="}
+-- >>> scryptEncryptedPassToPassHash encryptedPass
+-- PassHash {unPassHash = "14|8|1|YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXowMTIzNDU=|nENDaqWBmPKapAqQ3//H0iBImweGjoTqn5SvBS8Mc9FPFbzq6w65maYPZaO+SPamVZRXQjARQ8Y+5rhuDhjIhw=="}
+--
+-- prop> passHashToScryptEncryptedPass (scryptEncryptedPassToPassHash encPass) == encPass
+passHashToScryptEncryptedPass :: PassHash -> EncryptedPass
+passHashToScryptEncryptedPass (PassHash passHash) =
+  EncryptedPass $ encodeUtf8 passHash
+
+-- | Just like 'hashPassWithSalt', but generate a new 'Salt' everytime with a
+-- call to 'newSalt'.
+--
+-- >>> hashPass (Pass "foobar")
+-- PassHash {unPassHash = "14|8|1|...|..."}
 hashPass :: MonadIO m => Pass -> m PassHash
 hashPass pass = do
   salt <- liftIO newSalt
   pure $ hashPassWithSalt pass salt
 
+-- | Hash a password with the given 'Salt'.
+--
+-- The resulting 'PassHash' has the parameters used to hash it, as well as the
+-- 'Salt' appended to it, separated by @|@.
+--
+-- The input 'Salt' and resulting 'PassHash' are both byte-64 encoded.
+--
+-- >>> let salt = Salt "abcdefghijklmnopqrstuvwxyz012345"
+-- >>> hashPassWithSalt (Pass "foobar") salt
+-- PassHash {unPassHash = "14|8|1|YWJjZGVmZ2hpamtsbW5vcHFyc3R1dnd4eXowMTIzNDU=|nENDaqWBmPKapAqQ3//H0iBImweGjoTqn5SvBS8Mc9FPFbzq6w65maYPZaO+SPamVZRXQjARQ8Y+5rhuDhjIhw=="}
+--
+-- (Note that we use an explicit 'Salt' in the example above.  This is so that the
+-- example is reproducible, but in general you should use 'hashPass'.  'hashPass'
+-- generates a new 'Salt' everytime it is called.)
 hashPassWithSalt :: Pass -> Salt -> PassHash
-hashPassWithSalt (Pass pass) salt =
+hashPassWithSalt pass salt =
   scryptEncryptedPassToPassHash $
-    encryptPass defaultParams (passToScryptPass pass) salt
+    encryptPass defaultParams salt (passToScryptPass pass)
+
+data PassCheck = PassCheckSucc | PassCheckFail deriving (Eq, Read, Show)
+
+-- | Check a 'Pass' against a 'PassHash'.
+--
+-- Returns 'PassCheckSucc' on success.
+--
+-- >>> let salt = Salt "abcdefghijklmnopqrstuvwxyz012345"
+-- >>> let pass = Pass "foobar"
+-- >>> let passHash = hashPassWithSalt pass salt
+-- >>> checkPass pass passHash
+-- PassCheckSucc
+--
+-- Returns 'PassCheckFail' If an incorrect 'Pass' or 'PassHash' is used.
+--
+-- >>> let badpass = Pass "incorrect-password"
+-- >>> checkPass badpass passHash
+-- PassCheckFail
+--
+-- This should always fail if an incorrect password is given.
+--
+-- prop> let correctPassHash = hashPassWithSalt (Pass "foobar") salt in checkPass badpass correctPassHash == PassCheckFail
+checkPass :: Pass -> PassHash -> PassCheck
+checkPass pass passHash =
+  if verifyPass' (passToScryptPass pass) (passHashToScryptEncryptedPass passHash)
+  then PassCheckSucc
+  else PassCheckFail
