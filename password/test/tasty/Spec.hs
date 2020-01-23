fd53cc5aa705385b29e1ae830f3dbe9b178247f0
Felix Paulusma <felix.paulusma@gmail.com>
2020-01-24 00:25:31 +0100

    added Tasty tests for password library

--- password/test/tasty/Spec.hs
+++ password/test/tasty/Spec.hs
@@ -1,2 +1,27 @@
+import Crypto.Scrypt (Salt(..))
+import Data.Text.Encoding (encodeUtf8)
+import Test.Tasty
+import Test.Tasty.QuickCheck
+import Test.QuickCheck.Instances.Text ()
+
+import Data.Password
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = defaultMain $ testGroup "Password"
+  [ testProperty "Pass" $ \pass ->
+      unsafeShowPasswordText (mkPass pass) === pass
+  , testProperty "Scrypt (hashPass)" $ \pass -> ioProperty $ do
+      let pw = mkPass pass
+      hpw <- hashPass pw
+      return $ checkPass pw hpw === PassCheckSuccess
+  , testProperty "Scrypt (hashPass) fail" $ \pass pass2 -> ioProperty $ do
+      let pw = mkPass pass
+          pw2 = mkPass pass2
+          result = if pass == pass2 then PassCheckSuccess else PassCheckFail
+      hpw <- hashPass pw
+      return $ checkPass pw2 hpw === result
+  , testProperty "Scrypt (hashPassWithSalt)" $ \pass salt ->
+      let pw = mkPass pass
+          hpw = hashPassWithSalt (Salt $ encodeUtf8 salt) pw
+      in checkPass pw hpw === PassCheckSuccess
+  ]
