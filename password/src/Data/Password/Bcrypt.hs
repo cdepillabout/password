9fed4c524cdcfe779d9f7fc01a73b8d7f0ea1635
Felix Paulusma <felix.paulusma@gmail.com>
2020-03-16 01:19:11 +0100

    password/Bcrypt: tweaked documentation

--- password/src/Data/Password/Bcrypt.hs
+++ password/src/Data/Password/Bcrypt.hs
@@ -19,7 +19,7 @@ module Data.Password.Bcrypt (
   , checkPass
   -- * Hashing Manually (DISADVISED)
   --
-  -- If you have any doubt about what the parameters do or mean,
+  -- If you have any doubt about what the cost does or means,
   -- please, please just use 'hashPass'.
   , hashPassWithParams
   , hashPassWithSalt
@@ -50,7 +50,7 @@ data Bcrypt
 --
 -- >>> instance Arbitrary (Salt a) where arbitrary = Salt . pack <$> vector 16
 -- >>> instance Arbitrary Pass where arbitrary = fmap Pass arbitrary
--- >>> instance Arbitrary (PassHash Bcrypt) where arbitrary = hashPassWithSalt 10 <$> arbitrary <*> arbitrary
+-- >>> instance Arbitrary (PassHash Bcrypt) where arbitrary = hashPassWithSalt 8 <$> arbitrary <*> arbitrary
 
 -- | Hash the 'Pass' using the /bcrypt/ hash algorithm.
 --
@@ -60,17 +60,19 @@ hashPass :: MonadIO m => Pass -> m (PassHash Bcrypt)
 hashPass = hashPassWithParams 12
 
 -- | Hash a password with the given cost and also with the given 'Salt'
--- instead of generating a random salt.
+-- instead of generating a random salt. Using 'hashPassWithSalt' is strongly disadvised,
+-- and 'hashPassWithParams' should be used instead. /Never use a static salt/
+-- /in production applications!/
 --
 -- __N.B.__: The salt HAS to be 16 bytes or this function will throw an error!
 --
 -- >>> let salt = Salt "abcdefghijklmnop"
--- >>> hashPassWithSalt 12 salt (mkPass "foobar")
--- PassHash {unPassHash = "$2b$12$WUHhXETkX0fnYkrqZU3ta.pkddbc1r8CRUHGJyJ.siL0YkdRYfVuq"}
+-- >>> hashPassWithSalt 10 salt (mkPass "foobar")
+-- PassHash {unPassHash = "$2b$10$WUHhXETkX0fnYkrqZU3ta.N8Utt4U77kW4RVbchzgvBvBBEEdCD/u"}
 --
 -- (Note that we use an explicit 'Salt' in the example above.  This is so that the
 -- example is reproducible, but in general you should use 'hashPass'. 'hashPass'
--- generates a new 'Salt' everytime it is called.)
+-- (and 'hashPassWithParams') generates a new 'Salt' everytime it is called.)
 hashPassWithSalt
   :: Int -- ^ The cost parameter. Should be between 4 and 31 (inclusive). Values which lie outside this range will be adjusted accordingly.
   -> Salt Bcrypt -- ^ The salt. MUST be 16 bytes in length or an error will be raised.
@@ -82,8 +84,13 @@ hashPassWithSalt cost (Salt salt) (Pass pass) =
 
 -- | Hash a password using the /bcrypt/ algorithm with the given cost.
 --
--- __N.B.__: It is advised to use 'hashPass' if you're not sure what
--- the cost implies.
+-- The higher the cost, the longer 'hashPass' and 'checkPass' will take, thus
+-- increasing the security, but taking longer and taking up more resources.
+-- The optimal cost for generic user logins would be one that would take around
+-- 0.5 seconds to check on the machine that will run it.
+--
+-- __N.B.__: It is advised to use 'hashPass' if you're unsure about the
+-- implications that changing the cost brings with it.
 --
 -- @since 2.0.0.0
 hashPassWithParams
