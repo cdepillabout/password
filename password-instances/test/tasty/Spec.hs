1ff0910524053275ef58c593bc8a9963aaa45f4d
Felix Paulusma <felix.paulusma@gmail.com>
2020-03-30 23:01:28 +0200

    password-instances: added some unit tests just in case

--- password-instances/test/tasty/Spec.hs
+++ password-instances/test/tasty/Spec.hs
@@ -1,14 +1,55 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Aeson
+import Data.Aeson.Types (parseMaybe)
+import Data.Text (Text)
 import Database.Persist.Class (PersistField(..))
 import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck
 import Test.QuickCheck.Instances.Text ()
+import Web.HttpApiData (FromHttpApiData(..))
 
-import Data.Password (PassHash(..))
+import Data.Password (Pass, PassHash(..), unsafeShowPasswordText)
 import Data.Password.Instances()
+
 
 main :: IO ()
 main = defaultMain $ testGroup "Password Instances"
-  [ testProperty "PassHash (PersistField)" $ \pass ->
-      let pwd = PassHash pass
-      in fromPersistValue (toPersistValue pwd) === Right pwd
+  [ aesonTest
+  , fromHttpApiDataTest
+  , persistTest
   ]
+
+data TestUser = TestUser {
+  name :: Text,
+  password :: Pass
+} deriving (Show)
+
+instance FromJSON TestUser where
+  parseJSON = withObject "TestUser" $ \o ->
+    TestUser <$> o .: "name" <*> o .: "password"
+
+
+aesonTest :: TestTree
+aesonTest = testCase "Pass (Aeson)" $
+    assertEqual "password doesn't match" (Just testPass) $
+      unsafeShowPasswordText . password <$> parseMaybe parseJSON testUser
+  where
+    testPass = "testpass"
+    testUser = object
+      [ "name" .= String "testname"
+      , "password" .= String testPass
+      ]
+
+fromHttpApiDataTest :: TestTree
+fromHttpApiDataTest = testCase "Pass (FromHttpApiData)" $
+    assertEqual "password doesn't match" (Right testPass) $
+      unsafeShowPasswordText <$> parseUrlPiece testPass
+  where
+    testPass = "passtest"
+
+persistTest :: TestTree
+persistTest = testProperty "PassHash (PersistField)" $ \pass ->
+    let pwd = PassHash pass
+    in fromPersistValue (toPersistValue pwd) === Right pwd
