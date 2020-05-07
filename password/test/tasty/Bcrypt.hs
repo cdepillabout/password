05f71d43fe945e11a106e1d467b42e22ecccff2e
Felix Paulusma <felix.paulusma@gmail.com>
2020-05-08 00:14:49 +0200

    simplified tests, because we just use low rounds to speed it up, and "" and "\NUL" match on almost all algorithms

--- password/test/tasty/Bcrypt.hs
+++ password/test/tasty/Bcrypt.hs
@@ -1,19 +1,15 @@
 module Bcrypt where
 
-import Data.Text (Text)
 import Test.Tasty
-import Test.Tasty.QuickCheck
-import Test.QuickCheck.Instances.Text ()
 
-import Data.Password
 import Data.Password.Bcrypt
 
-import Internal (testCorrectPassword, testIncorrectPassword_, testWithSalt)
+import Internal (testCorrectPassword, testIncorrectPassword, testWithSalt)
 
 
 testBcrypt :: TestTree
 testBcrypt = testGroup "bcrypt"
   [ testCorrectPassword "Bcrypt (hashPassword)" (hashPasswordWithParams 4) checkPassword
-  , testIncorrectPassword_ "Bcrypt (hashPassword) fail" (hashPasswordWithParams 4) checkPassword
+  , testIncorrectPassword "Bcrypt (hashPassword) fail" (hashPasswordWithParams 4) checkPassword
   , testWithSalt "Bcrypt (hashPasswordWithSalt)" (hashPasswordWithSalt 4) checkPassword
   ]
--- password/test/tasty/Internal.hs
+++ password/test/tasty/Internal.hs
@@ -3,7 +3,6 @@
 module Internal where
 
 import Data.ByteArray (pack)
-import Data.Text (Text)
 import Test.Tasty
 import Test.Tasty.QuickCheck
 import Test.QuickCheck.Instances.Text ()
@@ -16,7 +15,7 @@ testCorrectPassword :: String
                     -> (Password -> PasswordHash a -> PasswordCheck)
                     -> TestTree
 testCorrectPassword s hashF checkF = testProperty s $
-  \pass -> run10 $ do
+  \pass -> ioProperty $ do
     let pw = mkPassword pass
     hpw <- hashF pw
     return $ checkF pw hpw === PasswordCheckSuccess
@@ -25,45 +24,25 @@ testIncorrectPassword :: String
                       -> (Password -> IO (PasswordHash a))
                       -> (Password -> PasswordHash a -> PasswordCheck)
                       -> TestTree
-testIncorrectPassword s hashF checkF =
-    testProperty s $ testIncorrectPassword' hashF checkF
-
--- Similar to 'testIncorrectPassword', but exempts the comparison of
--- "" and "\NUL", since 'bcrypt' and 'PBKDF2' match those as well.
-testIncorrectPassword_ :: String
-                       -> (Password -> IO (PasswordHash a))
-                       -> (Password -> PasswordHash a -> PasswordCheck)
-                       -> TestTree
-testIncorrectPassword_ s hashF checkF =
-    testProperty s $ \pass pass2 ->
-      not (all isEmpty [pass, pass2]) ==>
-        testIncorrectPassword' hashF checkF pass pass2
+testIncorrectPassword s hashF checkF = testProperty s $
+  \pass pass2 -> pass /= pass2 && not (all isEmpty [pass, pass2]) ==>
+    ioProperty $ do
+      let pw = mkPassword pass
+          pw2 = mkPassword pass2
+      hpw <- hashF pw
+      return $ checkF pw2 hpw === PasswordCheckFail
   where
     isEmpty c = c `elem` ["", "\NUL"]
-
-testIncorrectPassword' :: (Password -> IO (PasswordHash a))
-                       -> (Password -> PasswordHash a -> PasswordCheck)
-                       -> Text -> Text -> Property
-testIncorrectPassword' hashF checkF pass pass2 = run10 $ do
-    let pw = mkPassword pass
-        pw2 = mkPassword pass2
-        result = if pass == pass2 then PasswordCheckSuccess
-                                  else PasswordCheckFail
-    hpw <- hashF pw
-    return $ checkF pw2 hpw === result
 
 testWithSalt :: String
              -> (Salt a -> Password -> PasswordHash a)
              -> (Password -> PasswordHash a -> PasswordCheck)
              -> TestTree
 testWithSalt s hashWithSalt checkF = testProperty s $
-  \pass salt -> withMaxSuccess 10 $
+  \pass salt ->
     let pw = mkPassword pass
         hpw = hashWithSalt salt pw
     in checkF pw hpw === PasswordCheckSuccess
-
-run10 :: Testable prop => IO prop -> Property
-run10 = withMaxSuccess 10 . ioProperty
 
 instance Arbitrary (Salt a) where
   arbitrary = Salt . pack <$> vector 16
