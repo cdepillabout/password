f1a1973fcf957b4937a58acab881f8992e695419
Hiroto Shioi <shioihigg@gmail.com>
2020-05-18 23:29:11 +0900

    Add maximum password length policy

--- password/src/Data/Password/Validate.hs
+++ password/src/Data/Password/Validate.hs
@@ -1,11 +1,30 @@
 {-# LANGUAGE LambdaCase #-}
 
+{-|
+Module      : Data.Password.Valid
+Copyright   : (c) Dennis Gosnell, 2019; Felix Paulusma, 2020
+License     : BSD-style (see LICENSE file)
+Maintainer  : cdep.illabout@gmail.com
+Stability   : experimental
+Portability : POSIX
+
+= validate
+
+It is common for passwords to have a set of requirements.
+(e.g. Password must contain at least eight characters that consist of alphabetic
+characters combined with numbers or special characters.)
+
+Validate module provides set of functions which enables you to validate them.
+
+-}
+
 module Data.Password.Validate
   ( PasswordPolicy (..),
-    PolicyCharSet,
+    PolicyCharSet(..),
     FailedPolicies (..),
+    CharacterCategory(..),
     defaultPasswordPolicy,
-    validatePassword',
+    valid,
     validatePassword,
     specialChars,
   ) where
@@ -15,9 +34,6 @@ import Data.Maybe (catMaybes)
 import Data.Password.Internal (Password (..))
 import qualified Data.Text as T
 
--- TODO: Create code for checking that plain-text passwords conform to some sort of
--- password policy.
-
 -- $setup
 -- >>> :set -XOverloadedStrings
 --
@@ -25,67 +41,76 @@ import qualified Data.Text as T
 --
 -- >>> import Data.Password
 
+-- | Charset
 data PolicyCharSet = PolicyCharSetAscii
   deriving (Eq, Ord, Show)
 
+-- | Set of policies used to validate 'Password'
 data PasswordPolicy
   = PasswordPolicy
-      { -- | Required password length
-        passPolicyLength :: !Int,
-        -- | Required number of upper-case characters
-        passPolicyCharUpper :: !(Maybe Int),
-        -- | Required number of lower-case characters
-        passPolicyCharLower :: !(Maybe Int),
-        -- | Required number of special characters
+      { passPolicyMinimumLength :: !Int,
+        -- ^ Required password minimum length
+        passPolicyMaximumLength :: !Int,
+        -- ^ Required password maximum length
+        passPolicyCharUppercase :: !(Maybe Int),
+        -- ^ Required number of upper-case characters
+        passPolicyCharLowercase :: !(Maybe Int),
+        -- ^ Required number of lower-case characters
         passPolicyCharSpecial :: !(Maybe Int),
-        -- | Required number of ASCII-digit characters
+        -- ^ Required number of special characters
         passPolicyCharDigit :: !(Maybe Int),
-        -- | Set of characters that can be used for password
+        -- ^ Required number of ASCII-digit characters
         passPolicyCharSet :: PolicyCharSet
-      }
-  deriving (Eq, Ord, Show)
+        -- ^ Set of characters that can be used for password
+      } deriving (Eq, Ord, Show)
 
--- | Default parameters for the 'PasswordPolicy'
+-- | Default value for the 'PasswordPolicy'
 --
 -- >>> defaultPasswordPolicy
--- PasswordPolicy {passPolicyLength = 10, passPolicyCharUpper = Just 1, passPolicyCharLower = Just 1, passPolicyCharSpecial = Nothing, passPolicyCharDigit = Just 1, passPolicyCharSet = PolicyCharSetAscii}
+-- PasswordPolicy {passPolicyMinimumLength = 8, passPolicyMaximumLength = 30, passPolicyCharUppercase = Just 1, passPolicyCharLowercase = Just 1, passPolicyCharSpecial = Nothing, passPolicyCharDigit = Just 1, passPolicyCharSet = PolicyCharSetAscii}
 defaultPasswordPolicy :: PasswordPolicy
 defaultPasswordPolicy = PasswordPolicy
-  { passPolicyLength = 10,
-    passPolicyCharUpper = Just 1,
-    passPolicyCharLower = Just 1,
+  { passPolicyMinimumLength = 8,
+    passPolicyMaximumLength = 30,
+    passPolicyCharUppercase = Just 1,
+    passPolicyCharLowercase = Just 1,
     passPolicyCharSpecial = Nothing,
     passPolicyCharDigit = Just 1,
     passPolicyCharSet = PolicyCharSetAscii
   }
 
-data CharType
-  = Upper
-  | Lower
+-- | Character Category
+data CharacterCategory
+  = Uppercase
+  | Lowercase
   | Special
   | Digit
   deriving (Eq, Ord, Show)
 
 -- | Data type representing how password are invalid
 data FailedPolicies
-  = -- | Given password does not have enough length
-    NotEnoughLength Int Int
-  | -- | Given password does not fulfill one of the characther requirements
-    NotEnoughReqChars CharType Int Int
-  | -- | Invalid character was used
-    InvalidChar PolicyCharSet
+  = PasswordTooShort Int Int
+  -- ^ Password is too short
+  | PasswordTooLong Int Int
+  -- ^ Password is too long
+  | NotEnoughReqChars CharacterCategory Int Int
+  -- ^ Password does not contain characters that are required
+  | InvalidChar PolicyCharSet
+  -- ^ Password contains invalid character sets
   deriving (Eq, Ord, Show)
 
 -- | Check if given 'Password' fullfills all the Policies,
 -- return true if given password is valid
 --
+-- This is equivalent to @null $ validatePassword policy password@
+--
 -- >>> let pass = mkPassword "This_Is_Valid_PassWord1234"
--- >>> validatePassword' defaultPasswordPolicy pass
+-- >>> valid defaultPasswordPolicy pass
 -- True
-validatePassword' :: PasswordPolicy -> Password -> Bool
-validatePassword' policy password = null $ validatePassword policy password
+valid :: PasswordPolicy -> Password -> Bool
+valid policy pass = null $ validatePassword policy pass
 
--- | Check if given 'Password' fills all the Policies, returns list of
+-- | Check if given 'Password' fulfills all of the Policies, returns list of
 -- policies that failed.
 --
 -- >>> let pass = mkPassword "This_Is_Valid_Password1234"
@@ -94,40 +119,46 @@ validatePassword' policy password = null $ validatePassword policy password
 validatePassword :: PasswordPolicy -> Password -> [FailedPolicies]
 validatePassword passwordPolicy (Password password) =
   catMaybes
-    [ hasEnoughLength,
+    [ isTooShort,
+      isTooLong,
       isUsingPolicyCharSet,
-      hasRequiredChar (passPolicyCharUpper passwordPolicy) Upper,
-      hasRequiredChar (passPolicyCharLower passwordPolicy) Lower,
+      hasRequiredChar (passPolicyCharUppercase passwordPolicy) Uppercase,
+      hasRequiredChar (passPolicyCharLowercase passwordPolicy) Lowercase,
       hasRequiredChar (passPolicyCharSpecial passwordPolicy) Special,
       hasRequiredChar (passPolicyCharDigit passwordPolicy) Digit
     ]
   where
-    hasEnoughLength :: Maybe FailedPolicies
-    hasEnoughLength =
-      let policyLength = passPolicyLength passwordPolicy
-       in if T.length password > policyLength
+    isTooLong :: Maybe FailedPolicies
+    isTooLong =
+      let policyLength = passPolicyMaximumLength passwordPolicy
+      in if T.length password <= policyLength
+        then Nothing
+        else Just $ PasswordTooLong policyLength (T.length password)
+    isTooShort :: Maybe FailedPolicies
+    isTooShort =
+      let policyLength = passPolicyMinimumLength passwordPolicy
+       in if T.length password >= policyLength
             then Nothing
-            else Just $ NotEnoughLength (T.length password) policyLength
+            else Just $ PasswordTooShort policyLength (T.length password)
     isUsingPolicyCharSet :: Maybe FailedPolicies
     isUsingPolicyCharSet = case (passPolicyCharSet passwordPolicy) of
       PolicyCharSetAscii ->
         if T.all isAscii password
           then Nothing
           else Just $ InvalidChar PolicyCharSetAscii
-    hasRequiredChar :: Maybe Int -> CharType -> Maybe FailedPolicies
+    hasRequiredChar :: Maybe Int -> CharacterCategory -> Maybe FailedPolicies
     hasRequiredChar Nothing _ = Nothing
-    hasRequiredChar (Just requiredCharNum) charType =
-      let predicate = case charType of
-            Upper -> isUpper
-            Lower -> isLower
+    hasRequiredChar (Just requiredCharNum) characterCategory =
+      let predicate = case characterCategory of
+            Uppercase -> isUpper
+            Lowercase -> isLower
             Special -> (\char -> char `elem` specialChars)
             Digit -> isDigit
           passwordCharNum = T.length $ T.filter predicate password
-       in if passwordCharNum > requiredCharNum
+       in if passwordCharNum >= requiredCharNum
             then Nothing
-            else Just $ NotEnoughReqChars charType passwordCharNum requiredCharNum
+            else Just $ NotEnoughReqChars characterCategory requiredCharNum passwordCharNum
 
--- https://www.ipvoid.com/password-special-characters/
--- https://owasp.org/www-community/password-special-characters
+-- | Special characters
 specialChars :: [Char]
 specialChars = " !\"#$%&'()*+,-./:;<=>?@[]^_`{|}~"
