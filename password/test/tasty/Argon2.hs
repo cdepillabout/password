62df168c1f367668811327be01bb6ce3d6776471
Felix Paulusma <felix.paulusma@gmail.com>
2021-02-14 15:03:28 +0100

    password/test/Argon2: added unit tests to make sure with and without padding checks out

--- password/test/tasty/Argon2.hs
+++ password/test/tasty/Argon2.hs
@@ -1,7 +1,8 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Argon2 where
 
 import Test.Tasty
-import Test.QuickCheck.Instances.Text ()
+import Test.Tasty.HUnit (assertBool, testCase)
 
 import Data.Password.Argon2
 
@@ -17,6 +18,7 @@ testArgon2 = testGroup "Argon2"
                  checkPassword
   , testWithParams "Argon2 (Argon2i)" $ fastParams{ argon2Variant = Argon2i }
   , testWithParams "Argon2 (Argon2d)" $ fastParams{ argon2Variant = Argon2d }
+  , paddingTests
   ]
   where
     testWithParams s params =
@@ -27,3 +29,19 @@ testArgon2 = testGroup "Argon2"
         argon2MemoryCost = 2 ^ (8 :: Int),
         argon2TimeCost = 1
       }
+
+paddingTests :: TestTree
+paddingTests = testGroup "Padding"
+    [ testCase "with padding" $
+        assertBool "Bad hash" $ checkPassword pass testHash1 == PasswordCheckSuccess
+    , testCase "without padding" $
+        assertBool "Bad hash" $ checkPassword pass testHash2 == PasswordCheckSuccess
+    ]
+
+pass :: Password
+pass = "foobar"
+
+-- Hashed password ("foobar") with salt ("abcdefghijklmnop")
+testHash1, testHash2 :: PasswordHash Argon2
+testHash1 = PasswordHash "$argon2id$v=19$m=65536,t=2,p=1$YWJjZGVmZ2hpamtsbW5vcA==$BztdyfEefG5V18ZNlztPrfZaU5duVFKZiI6dJeWht0o="
+testHash2 = PasswordHash "$argon2id$v=19$m=65536,t=2,p=1$YWJjZGVmZ2hpamtsbW5vcA$BztdyfEefG5V18ZNlztPrfZaU5duVFKZiI6dJeWht0o"
