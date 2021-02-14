8f30e87d3efb3477d31f72ea3a5fa730f06c7029
Felix Paulusma <felix.paulusma@gmail.com>
2021-02-14 15:32:16 +0100

    old GHCs need Semigroup import

--- password/src/Data/Password/Internal.hs
+++ password/src/Data/Password/Internal.hs
@@ -36,6 +36,9 @@ import Data.ByteArray (Bytes, constEq, convert)
 import Data.ByteString (ByteString)
 import Data.Function (on)
 import Data.ByteString.Base64 (decodeBase64)
+#if! MIN_VERSION_base(4,13,0)
+import Data.Semigroup ((<>))
+#endif
 import Data.String (IsString(..))
 import Data.Text as T (
     Text,
