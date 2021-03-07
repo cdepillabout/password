a832123a7fe7b08d7e655a179926c724e241c1ed
Felix Paulusma <felix.paulusma@gmail.com>
2021-03-07 17:08:46 +0100

    password/test: added reference test for Argon2

--- password/test/tasty/Argon2.hs
+++ password/test/tasty/Argon2.hs
@@ -2,7 +2,7 @@
 module Argon2 where
 
 import Test.Tasty
-import Test.Tasty.HUnit (assertBool, testCase)
+import Test.Tasty.HUnit (assertBool, assertEqual, testCase)
 
 import Data.Password.Argon2
 
@@ -11,7 +11,8 @@ import Internal
 
 testArgon2 :: TestTree
 testArgon2 = testGroup "Argon2"
-  [ testCorrectPassword "Argon2 (hashPassword)" hashFast checkPassword
+  [ referenceTest
+  , testCorrectPassword "Argon2 (hashPassword)" hashFast checkPassword
   , testIncorrectPassword "Argon2 (hashPassword) fail" hashFast checkPassword
   , testWithSalt "Argon2 (hashPasswordWithSalt)"
                  (hashPasswordWithSalt fastParams)
@@ -45,3 +46,19 @@ pass = "foobar"
 hashWithPadding, hashWithoutPadding :: PasswordHash Argon2
 hashWithPadding    = PasswordHash "$argon2id$v=19$m=65536,t=2,p=1$YWJjZGVmZ2hpamtsbW5vcA==$BztdyfEefG5V18ZNlztPrfZaU5duVFKZiI6dJeWht0o="
 hashWithoutPadding = PasswordHash "$argon2id$v=19$m=65536,t=2,p=1$YWJjZGVmZ2hpamtsbW5vcA$BztdyfEefG5V18ZNlztPrfZaU5duVFKZiI6dJeWht0o"
+
+-- Reference check using the Command-line Utility output example
+-- from: https://github.com/P-H-C/phc-winner-argon2
+referenceTest :: TestTree
+referenceTest = testCase "PHC Argon2 reference" $
+    assertEqual "output hash is wrong" expected $
+        hashPasswordWithSalt params salt pwd
+  where
+    expected = PasswordHash "$argon2i$v=19$m=65536,t=2,p=4$c29tZXNhbHQ$RdescudvJCsgt3ub+b+dWRWJTmaaJObG"
+    salt = Salt "somesalt"
+    pwd = mkPassword "password"
+    params = defaultParams {
+        argon2Variant = Argon2i,
+        argon2Parallelism = 4,
+        argon2OutputLength = 24
+    }
