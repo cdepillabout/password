62ddb03c8fefd95c79baaa8a72493109812e87a5
Felix Paulusma <felix.paulusma@gmail.com>
2022-02-14 00:10:30 +0100

    Argon2/test: unit tests to check hashes without version are checked like they were version 1.0

--- password/test/tasty/Argon2.hs
+++ password/test/tasty/Argon2.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
-module Argon2 where
+module Argon2 (testArgon2) where
 
-import Test.Tasty
+import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.HUnit (assertBool, assertEqual, testCase)
 
 import Data.Password.Argon2
@@ -20,6 +20,7 @@ testArgon2 = testGroup "Argon2"
   , testWithParams "Argon2 (Argon2i)" $ fastParams{ argon2Variant = Argon2i }
   , testWithParams "Argon2 (Argon2d)" $ fastParams{ argon2Variant = Argon2d }
   , paddingTests
+  , omittedVersionTest
   ]
   where
     testWithParams s params =
@@ -47,14 +48,28 @@ hashWithPadding, hashWithoutPadding :: PasswordHash Argon2
 hashWithPadding    = PasswordHash "$argon2id$v=19$m=65536,t=2,p=1$YWJjZGVmZ2hpamtsbW5vcA==$BztdyfEefG5V18ZNlztPrfZaU5duVFKZiI6dJeWht0o="
 hashWithoutPadding = PasswordHash "$argon2id$v=19$m=65536,t=2,p=1$YWJjZGVmZ2hpamtsbW5vcA$BztdyfEefG5V18ZNlztPrfZaU5duVFKZiI6dJeWht0o"
 
+-- Very old hashes might not have version parts, so infer as version 1.0
+omittedVersionTest :: TestTree
+omittedVersionTest = testGroup "Version 1.0"
+    [ go "version 1.0 part in hash (placebo)" "testtest" v10Hash
+    , go "no version part in hash == version 1.0" "testtest" v10HashWithoutVersion
+    , go "version 1.3 part in hash (reference)" "password" referenceHash
+    , testCase "no version 1.3 part in hash should fail" $
+        assertEqual "check passed!?" PasswordCheckFail $
+            checkPassword "password" referenceHashWithoutVersion
+    ]
+  where
+    go s p = testCase s
+        . assertEqual "check failed" PasswordCheckSuccess
+        . checkPassword p
+
 -- Reference check using the Command-line Utility output example
 -- from: https://github.com/P-H-C/phc-winner-argon2
 referenceTest :: TestTree
 referenceTest = testCase "PHC Argon2 reference" $
-    assertEqual "output hash is wrong" expected $
+    assertEqual "output hash is wrong" referenceHash $
         hashPasswordWithSalt params salt pwd
   where
-    expected = PasswordHash "$argon2i$v=19$m=65536,t=2,p=4$c29tZXNhbHQ$RdescudvJCsgt3ub+b+dWRWJTmaaJObG"
     salt = Salt "somesalt"
     pwd = mkPassword "password"
     params = defaultParams {
@@ -62,3 +77,11 @@ referenceTest = testCase "PHC Argon2 reference" $
         argon2Parallelism = 4,
         argon2OutputLength = 24
     }
+
+-- Weirdly lined out to show it's exactly the same, except the 'v=' part is missing.
+referenceHash, referenceHashWithoutVersion :: PasswordHash Argon2
+referenceHash          = PasswordHash "$argon2i$v=19$m=65536,t=2,p=4$c29tZXNhbHQ$RdescudvJCsgt3ub+b+dWRWJTmaaJObG"
+referenceHashWithoutVersion = PasswordHash "$argon2i$m=65536,t=2,p=4$c29tZXNhbHQ$RdescudvJCsgt3ub+b+dWRWJTmaaJObG"
+v10Hash, v10HashWithoutVersion :: PasswordHash Argon2
+v10Hash          = PasswordHash "$argon2i$v=16$m=65536,t=2,p=1$Kx1BEcpIg0Ey5GyXq5do2w$0qRfWHw09EdqQkSsaG57O/ou8v/E6Vc83w"
+v10HashWithoutVersion = PasswordHash "$argon2i$m=65536,t=2,p=1$Kx1BEcpIg0Ey5GyXq5do2w$0qRfWHw09EdqQkSsaG57O/ou8v/E6Vc83w"
