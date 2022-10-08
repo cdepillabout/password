d89129a6071ab38041f0d7f8f367f18f0e6e6cce
Felix Paulusma <felix.paulusma@gmail.com>
2022-10-09 01:16:31 +0200

    password: lowered bcrypt second hash/parse test to 5 for speed

--- password/test/tasty/Bcrypt.hs
+++ password/test/tasty/Bcrypt.hs
@@ -10,7 +10,7 @@ import Internal
 testBcrypt :: TestTree
 testBcrypt = testGroup "bcrypt"
   [ testCorrectPassword "Bcrypt (hashPassword)" (hashPasswordWithParams 4) checkPassword extractParams 4
-  , testCorrectPassword "Bcrypt (hashPassword)" (hashPasswordWithParams 8) checkPassword extractParams 8
+  , testCorrectPassword "Bcrypt (hashPassword 2)" (hashPasswordWithParams 5) checkPassword extractParams 5
   , testIncorrectPassword "Bcrypt (hashPassword) fail" (hashPasswordWithParams 4) checkPassword
   , testWithSalt "Bcrypt (hashPasswordWithSalt)" (hashPasswordWithSalt 4) checkPassword extractParams 4
   ]
