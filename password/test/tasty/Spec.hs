e5f6100217ace76e1dd44cd66b7dd8c764af2479
Isaac van Bakel <ivb@vanbakel.io>
2023-02-08 15:56:32 +0000

    Put all hashing algorithms behind Cabal flags
    This allows a developer depending on `password` to toggle on and off
    support for different password-hashing algorithms at the package level.
    
    The motivator for this is that `cryptonite`'s Argon2 implementation is
    broken on M1 Macs (or any `aarch64-darwin` machine), and in the Nix
    package set, `cryptonite` has been patched so that any calls to the
    Argon2 implementation will cause a compiler error telling the developer
    that it is broken. However, as a user of `password`, I don't use Argon2,
    and I still want to be able to compile `password` by simply ignoring
    Argon2 as a possible hashing algorithm. That's what these flags allow me
    to do.

--- password/test/tasty/Spec.hs
+++ password/test/tasty/Spec.hs
@@ -1,13 +1,23 @@
+{-# LANGUAGE CPP #-}
+
 import Test.Tasty
 import Test.Tasty.QuickCheck
 import Test.Tasty.Runners (NumThreads(..))
 
 import Data.Password.Types
 
+#ifdef CABAL_FLAG_argon2
 import Argon2 (testArgon2)
+#endif
+#ifdef CABAL_FLAG_bcrypt
 import Bcrypt (testBcrypt)
+#endif
+#ifdef CABAL_FLAG_pbkdf2
 import PBKDF2 (testPBKDF2)
+#endif
+#ifdef CABAL_FLAG_scrypt
 import Scrypt (testScrypt)
+#endif
 import Validate (testValidate)
 
 main :: IO ()
@@ -15,9 +25,17 @@ main = defaultMain $ localOption (NumThreads 1) $
   testGroup "Password"
     [ testProperty "Password" $ \pass ->
         unsafeShowPassword (mkPassword pass) === pass
+#ifdef CABAL_FLAG_argon2
     , testArgon2
+#endif
+#ifdef CABAL_FLAG_bcrypt
     , testBcrypt
+#endif
+#ifdef CABAL_FLAG_pbkdf2
     , testPBKDF2
+#endif
+#ifdef CABAL_FLAG_scrypt
     , testScrypt
+#endif
     , testValidate
     ]
