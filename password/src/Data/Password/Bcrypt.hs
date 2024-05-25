904d5ee77df7ae64452211f4018373437fc1b742
Felix Paulusma <felix.paulusma@gmail.com>
2024-05-26 00:58:19 +0200

    password: small tweak to 'Bcrypt.extractParams'

--- password/src/Data/Password/Bcrypt.hs
+++ password/src/Data/Password/Bcrypt.hs
@@ -221,8 +221,8 @@ checkPassword pass (PasswordHash passHash) =
 extractParams :: PasswordHash Bcrypt -> Maybe Int
 extractParams (PasswordHash passHash) =
   case T.split (== '$') passHash of
-    [_, version, cost, _pass] -> do
-      guard $ elem version $ map T.pack ["2", "2a", "2x", "2y", "2b"]
+    ["", version, cost, _pass] -> do
+      guard $ elem version $ map T.pack ["2b", "2y", "2x", "2a", "2"]
       readMaybe $ T.unpack cost
     _ -> Nothing
 
